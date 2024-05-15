5b2cd3bf3143a98b0828858929f379f4aeb714b1
fabricio <fzabra@gmail.com>
2024-05-15 09:35:28 -0300

    fix render

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { GlobalStyles } from './GlobalStyled.style';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
 
+root.render(
+  <>
+  <GlobalStyles />
+  <App />
+  </>  
+)
 reportWebVitals();
